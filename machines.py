072c6bd7d66662ad333a6cb6d59f0d84999c2c4c
George Wong <gnwong2@illinois.edu>
2017-10-05 19:04:39 -0500

    Made build script compatible with python3 and added bh config.

--- machines.py
+++ machines.py
@@ -11,17 +11,22 @@ def add_machine(name, compiler, c_flags, l_flags, gsl_dir):
   machine['GSL_DIR'] = gsl_dir
   machines[name] = machine
 
+def get_machine():
+  for key in machines:
+    if os.uname()[1] == machines[key]['NAME']:
+      return machines[os.uname()[1]]
+  print('UNKNOWN MACHINE ' + os.uname()[1])
+  sys.exit()
+
 add_machine(name='meade', 
             compiler='h5pcc',  
             c_flags='-Ofast -Wall -Werror -fdiagnostics-color -fopenmp',
             l_flags='',
             gsl_dir='/home/brryan/Software/gsl')
 
-def get_machine():
-  for key in machines:
-    print key
-    if os.uname()[1] == machines[key]['NAME']:
-      return machines[os.uname()[1]]
-  print 'UNKNOWN MACHINE ' + os.uname()[1]
-  sys.exit()
+add_machine(name='bh',
+            compiler='h5pcc',
+            c_flags='-Ofast -std=c99 -Wall -fopenmp -g -DVERSION=\"$(GIT_VERSION)\"',
+            l_flags='-lm -lgsl -lgslcblas',
+            gsl_dir='')
 
