7655c9abc3cbe5c66e815d0cb34fa64bbd9b2897
Chi-kwan Chan <chanc@email.arizona.edu>
2020-05-03 20:24:06 -0700

    Reorder parameter reading in "src/par.c" to match "src/par.h"
    This is again for readability and would not affect the functionality.

--- src/par.c
+++ src/par.c
@@ -14,41 +14,43 @@ void load_par (const char *fname, Params *params) {
   }
 
   // set default values here
+  params->biasTuning = 1.;
+  params->fitBiasNs = 10000.;
+  params->fitBias = 0.;
+
   params->TP_OVER_TE = 3.;
+  params->beta_crit = 1.;
   params->trat_small = 1.;
   params->trat_large = 10.;
-  params->biasTuning = 1.;
-  params->fitBias = 0.;
-  params->beta_crit = 1.;
-  params->fitBiasNs = 10000.;
   params->Thetae_max = 1.e100;
 
   // modify parameters/types below
   while (fgets(line, 255, fp) != NULL) {
 
-    if (line[0] == '#') continue;
+    if (line[0] == '#') continue; 
 
     read_param(line, "Ns", &(params->Ns), TYPE_DBL);
     read_param(line, "MBH", &(params->MBH), TYPE_DBL);
     read_param(line, "M_unit", &(params->M_unit), TYPE_DBL);
-    read_param(line, "bias", &(params->biasTuning), TYPE_DBL);
-    read_param(line, "fit_bias", &(params->fitBias), TYPE_INT);
-    read_param(line, "fit_bias_ns", &(params->fitBiasNs), TYPE_DBL);
-    read_param(line, "Thetae_max", &(params->Thetae_max), TYPE_DBL);
+    read_param(line, "dump", (void *)(params->dump), TYPE_STR);
+    read_param(line, "spectrum", (void *)(params->spectrum), TYPE_STR);
 
+    // bias
+    read_param(line, "bias", &(params->biasTuning), TYPE_DBL);
+    read_param(line, "fit_bias_ns", &(params->fitBiasNs), TYPE_DBL);
+    read_param(line, "fit_bias", &(params->fitBias), TYPE_INT);
+
+    // two point model
+    read_param(line, "lnumin", &(params->lnumin), TYPE_DBL);
+    read_param(line, "lnumax", &(params->lnumax), TYPE_DBL);
+    read_param(line, "alpha_spec", &(params->alpha_spec), TYPE_DBL);    
+
+    // electron temperature models
     read_param(line, "TP_OVER_TE", &(params->TP_OVER_TE), TYPE_DBL);
     read_param(line, "beta_crit", &(params->beta_crit), TYPE_DBL);
     read_param(line, "trat_small", &(params->trat_small), TYPE_DBL);
     read_param(line, "trat_large", &(params->trat_large), TYPE_DBL);
-
-    read_param(line, "dump", (void *)(params->dump), TYPE_STR);
-    read_param(line, "spectrum", (void *)(params->spectrum), TYPE_STR);
-    
-    // two point model
-    read_param(line, "lnumin", &(params->lnumin), TYPE_DBL);
-    read_param(line, "lnumax", &(params->lnumax), TYPE_DBL);
-    read_param(line, "alpha_spec", &(params->alpha_spec), TYPE_DBL);
-    
+    read_param(line, "Thetae_max", &(params->Thetae_max), TYPE_DBL);
   }
 
   fclose(fp);
